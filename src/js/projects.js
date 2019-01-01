68d0157f94f97889924b10864144b95600f876a8
Sheldon Led <sheldonled@gmail.com>
2019-01-01 19:46:32 +0000

    new year \o/

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,7 +2,7 @@ export default [
   {
     "name":"Easy GD",
     "img":"img/easygd.png",
-    "url":"http://github.sheldonled.com/easygd/",
+    "url":"https://github.sheldonled.com/easygd/",
     "vistProjMsg": {
       "en": "visit project",
       "pt-BR": "visitar projeto",
@@ -26,7 +26,7 @@ export default [
   {
     "name":"Hey VR!",
     "img":"img/mozvr.png",
-    "url":"http://github.sheldonled.com/heyvr",
+    "url":"https://github.sheldonled.com/heyvr",
     "vistProjMsg": {
       "en": "visit project",
       "pt-BR": "visitar projeto",
@@ -38,17 +38,17 @@ export default [
     "description": {
       "en": `<p>It's a pool of examples using VR framworks. By the moment the only Framework
           used is <a href='https://aframe.io' target='_blank'>A-Frame</a>.</p>
-          <p><a href='http://github.sheldonled.com/heyvr/myhelloworld.html' target='_blank'>See hello world example</a>.</p>`,
+          <p><a href='https://github.sheldonled.com/heyvr/myhelloworld.html' target='_blank'>See hello world example</a>.</p>`,
 
       "pt-BR": `<p>É apenas um repositório de exemplos usando biblitecas de Realidade Virtual.
         No momento a única biblioeca utilizada é o <a href='https://aframe.io' target='_blank'>A-Frame</a>.</p>
-        <p><a href='http://github.sheldonled.com/heyvr/myhelloworld.html' target='_blank'>Veja meu 'hello world'</a>.</p>`,
+        <p><a href='https://github.sheldonled.com/heyvr/myhelloworld.html' target='_blank'>Veja meu 'hello world'</a>.</p>`,
     },
   },
   {
     "name":"FootNotes",
     "img":"img/footnotes.png",
-    "url":"http://github.sheldonled.com/footnotes",
+    "url":"https://github.sheldonled.com/footnotes",
     "vistProjMsg": {
       "en": "visit project",
       "pt-BR": "visitar projeto",
@@ -60,17 +60,17 @@ export default [
     "description": {
       "en": `<p>A piece of JavaScript/JQuery code that shows notes about
         something in your website. The best way to understand is to
-        <a href='http://github.sheldonled.com/footnotes' target='_blank'>Take a look</a>.</p>`,
+        <a href='https://github.sheldonled.com/footnotes' target='_blank'>Take a look</a>.</p>`,
 
       "pt-BR": `<p>Um pedaço de código JavaScript/JQuery que mostra notas sobre
         alguma coisa no seu site. A melhor forma de entender este projeto é
-        <a href='http://github.sheldonled.com/footnotes' target='_blank'>Ver uma demonstração online</a>.</p>`,
+        <a href='https://github.sheldonled.com/footnotes' target='_blank'>Ver uma demonstração online</a>.</p>`,
     },
   },
   {
     "name":"Coplacar",
     "img":"img/coplacar.png",
-    "url":"http://github.sheldonled.com/coplacar",
+    "url":"https://github.sheldonled.com/coplacar",
     "vistProjMsg": {
       "en": "visit project",
       "pt-BR": "visitar projeto",
@@ -94,7 +94,7 @@ export default [
   {
     "name":"Play Compras",
     "img":"img/playcompras.png",
-    "url":"http://github.sheldonled.com/playcompras",
+    "url":"https://github.sheldonled.com/playcompras",
     "vistProjMsg": {
       "en": "visit project",
       "pt-BR": "visitar projeto",
@@ -111,7 +111,7 @@ export default [
   {
     "name":"Music Fun",
     "img":"img/musicfun.png",
-    "url":"http://github.sheldonled.com/musicfun",
+    "url":"https://github.sheldonled.com/musicfun",
     "vistProjMsg": {
       "en": "visit project",
       "pt-BR": "visitar projeto",
@@ -128,7 +128,7 @@ export default [
   {
     "name":"Alfa Notas",
     "img":"img/alfanotas.png",
-    "url":"http://github.sheldonled.com/alfanotas",
+    "url":"https://github.sheldonled.com/alfanotas",
     "vistProjMsg": {
       "en": "visit project",
       "pt-BR": "visitar projeto",
@@ -138,8 +138,8 @@ export default [
       "pt-BR": "veja mais",
     },
     "description": {
-      "en": `<p>A webapp that allows students from <a href='http://www.alfa.br' target='_blank'>Faculdade Alfa</a> calculate their grade.</p>`,
-      "pt-BR": `<p>Uma WebApp que permite os estudantes da <a href='http://www.alfa.br' target='_blank'>Faculdade Alfa</a> calcularem suas notas.</p>`,
+      "en": `<p>A webapp that allows students from <a href='https://www.alfa.br' target='_blank'>Faculdade Alfa</a> calculate their grade.</p>`,
+      "pt-BR": `<p>Uma WebApp que permite os estudantes da <a href='https://www.alfa.br' target='_blank'>Faculdade Alfa</a> calcularem suas notas.</p>`,
     },
   }
 ];
